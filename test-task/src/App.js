3ea69b52e86f20df33e1c65d2d9fe79df7ff1f01
Anton Volkov <volkovanton71@yandex.ru>
2020-05-16 16:02:22 +0300

    created container Marketings

---
+++ test-task/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import Marketings from './containers/Marketings/Marketings'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Marketings />
+    )
+  }
+}
---
+++ test-task/src/containers/Marketings/Marketings.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import classes from './Marketings.css'
+
+export default class Marketings extends Component {
+
+  state = {
+
+  }
+
+
+  render() {
+    return (
+      <div className={ classes.Marketings}>
+        dfdfdff
+      </div>
+    )
+  }
+}
